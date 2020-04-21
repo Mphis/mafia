e9041f4ffa7378eb09ef74800249021d073ab38a
Pranavi Choudhary <ppch09@gmail.com>
2020-04-21 23:56:51 +0530

    Updates

---
+++ precache-manifest.14657bdf7a4e94df8f35e2984856b7d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9af8d291b5cd7ba857fa90e19aba34",
+    "url": "/mafia/index.html"
+  },
+  {
+    "revision": "efc1cbb829fc463f2c23",
+    "url": "/mafia/static/css/main.0f983f7b.chunk.css"
+  },
+  {
+    "revision": "04853ce29a95f90b02f1",
+    "url": "/mafia/static/js/2.a3fb700f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mafia/static/js/2.a3fb700f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc1cbb829fc463f2c23",
+    "url": "/mafia/static/js/main.1cb7caae.chunk.js"
+  },
+  {
+    "revision": "e06d44aced886fe45520",
+    "url": "/mafia/static/js/runtime-main.54b7de88.js"
+  }
+]);
